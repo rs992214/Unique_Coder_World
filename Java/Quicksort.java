e5fde7f3ea37079dc499cbe4d69c45586f426363
abhicreation19 <72510691+abhicreation19@users.noreply.github.com>
2020-10-07 20:22:47 +0530

    Create Quicksort.java

---
+++ Java/Quicksort.java
@@ -0,0 +1,21 @@
+static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+  
+    // Driver program 
+    public static void main(String args[]) 
+    { 
+        int arr[] = {10, 7, 8, 9, 1, 5}; 
+        int n = arr.length; 
+  
+        QuickSort ob = new QuickSort(); 
+        ob.sort(arr, 0, n-1); 
+  
+        System.out.println("sorted array"); 
+        printArray(arr); 
+    } 
+} 
