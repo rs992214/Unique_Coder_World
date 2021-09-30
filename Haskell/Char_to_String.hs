faabda188bbbb267cdfa7efc24ed430b99f3d475
Firemit <91111039+Firemit@users.noreply.github.com>
2021-10-01 00:10:01 +0530

    Create Char_to_String.hs

---
+++ Haskell/Char_to_String.hs
@@ -0,0 +1,10 @@
+Prelude> :load StringUtil.hs
+[1 of 1] Compiling Main             ( StringUtil.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> 
+*Main> charToString 'a'
+"a"
+*Main> 
+*Main> charToString '1'
+"1"
+*Main>
