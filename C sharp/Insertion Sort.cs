5b250ec3d67540bb2875b1421ea8fa66dc4f048b
PRAKHAR SAHU <80202909+PrakharSahu02@users.noreply.github.com>
2021-10-01 13:19:58 +0530

    Create Insertion Sort.cs

---
+++ C sharp/Insertion Sort.cs
@@ -0,0 +1,34 @@
+using System;
+ 
+class InsertionSort {
+    void sort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+ 
+    static void printArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+ 
+        Console.Write("\n");
+    }
+ 
+    public static void Main()
+    {
+        int[] arr = { 12, 11, 13, 5, 6 };
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        printArray(arr);
+    }
+}
