760daeb1779cd52ed1c365940efac32b27fe922a
Manas Harsh <33492814+ManasHarsh@users.noreply.github.com>
2021-10-01 13:14:33 +0530

    Update primeNo.py

--- Python/primeNo.py
+++ Python/primeNo.py
@@ -14,7 +14,7 @@ if num > 1:
            print(i,"times",num//i,"is",num)
            break
    else:
-       print(num,"is a prime number")
+       print(num,"is a hacking number")
        
 # if input number is less than
 # or equal to 1, it is not prime
