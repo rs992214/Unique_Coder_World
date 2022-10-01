698ddb98a4be152e97bac389f783a60a54b45fdb
askaintura <114813437+askaintura@users.noreply.github.com>
2022-10-01 20:34:50 +0530

    Update Evenodd.py

--- Python/Evenodd.py
+++ Python/Evenodd.py
@@ -1,6 +1,7 @@
 # Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
+# If not remember please contact me.
 
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
