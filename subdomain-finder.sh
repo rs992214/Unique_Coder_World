c2071060b36589f81334203416d021c18579f6ff
vinay7751 <64535781+vinay7751@users.noreply.github.com>
2022-10-01 22:23:00 +0530

    Update subdomain-finder.sh

--- subdomain-finder.sh
+++ subdomain-finder.sh
@@ -10,30 +10,30 @@ echo -e "${GREEN}[+]Subdomain Enumeretion"
 
 #Assetfinder
 echo -e "${GREEN}[+]Starting Assetfinder"
-assetfinder --subs-only $1 |sort -u |tee assetfinder.txt
+assetfinder --subs-only $1 |sort -u |tee assetfinder.txt | notify
 
 #Sublist3r
 echo -e "${GREEN}[+]Starting Sublist3r"
-python ~/recon/subdomain/Sublist3r/sublist3r.py -d $1 -o sublister.txt
+python ~/recon/subdomain/Sublist3r/sublist3r.py -d $1 -o sublister.txt | notify
 
 #Filtering
 echo -e "${GREEN}[+]Starting Filtering"
-cat sublist3r.txt assetfinder.txt | sort -u |uniq -u| grep -v "*" |sort -u|tee FinalSubs.txt
+cat sublist3r.txt assetfinder.txt | sort -u |uniq -u| grep -v "*" |sort -u|tee FinalSubs.txt | notify
 
 #Httprobe
 echo -e "${GREEN}[+]Starting Httprobe"
-cat Final-Subs.txt |sort -u |uniq -u|httprobe|tee $1-alive.txt
+cat Final-Subs.txt |sort -u |uniq -u|httprobe|tee $1-alive.txt | notify
 
 #Get-Tilie
 echo -e "${GREEN}[+]Starting Get-titles"
-cat $1-alive.txt|get-titles
+cat $1-alive.txt|get-titles | notify
 
 echo -e "${GREEN}[+]Start Subdomain Tackeover Scan"
-subjack -w Final-Subs.txt -t 20 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o subjack.txt
-subzy -targets Final-Subs.txt -hide_fails --verify_ssl -concurrency 20 |sort -u|tee "subzy.txt"
+subjack -w Final-Subs.txt -t 20 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o subjack.txt | notify
+subzy -targets Final-Subs.txt -hide_fails --verify_ssl -concurrency 20 |sort -u|tee "subzy.txt" | notify
 
 echo -e "${GREEN}[+]Aquatone Screenshot"
-cat $1-alive.txt| aquatone -screenshot-timeout 10 -out screenshots/
+cat $1-alive.txt| aquatone -screenshot-timeout 10 -out screenshots/ | notify
 
 echo -e "${GREEN}[+]Finish"
 
