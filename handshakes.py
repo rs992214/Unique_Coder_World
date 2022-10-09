e85788bdbb83dfaacaa984b17a76762b650a2ecb
AhonaGhosal16 <102255929+AhonaGhosal16@users.noreply.github.com>
2022-10-09 14:05:00 +0530

    Create handshakes.py

---
+++ handshakes.py
@@ -0,0 +1,8 @@
+# number of people
+N = 30
+
+# formula
+no_of_handshakes = int(N * ((N - 1) / 2))
+
+# print number of handshakes
+print('Maximum number of handshakes possible for', N, 'pople are', no_of_handshakes)
