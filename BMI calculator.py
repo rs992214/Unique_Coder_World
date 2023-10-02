08b2b2868f6a5dc3893d9baa7ac8590c52e4e87e
whizz992215 <72332867+whizz992215@users.noreply.github.com>
2023-10-02 10:10:02 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -8,7 +8,7 @@ print("Your Body Mass Index is", the_BMI)
 # using the if-elif-else conditions  
 if the_BMI <= 18.5:  
     print("Oops! You are underweight.")  
-elif the_BMI <= 24.9:  
+elif the_BMI <= 22.9:  
     print("Awesome! You are healthy.")  
 elif the_BMI <= 29.9:  
     the_print("Eee! You are over weight.")  
